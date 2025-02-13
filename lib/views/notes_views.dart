cd99e0758c8bd3f77c8053297ba2686bc5adf116
abdelgayed6 <abdelgayed277@gmail.com>
2025-02-13 16:26:19 +0200

    create notes view

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/notes_view_body.dart';
+
 class NotesViews extends StatelessWidget {
   const NotesViews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
